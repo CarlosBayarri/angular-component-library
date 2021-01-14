a6c366340944df3445ceeb6eb06f10e68a091969
carlosbayarri <carlobr7@gmail.com>
2021-01-14 15:33:32 +0100

    changes in lib to test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from '../../projects/components/src/lib/components.module';
 import { ButtonModule } from '../../projects/button/src/lib/button.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { NgxMatTypeaheadModule } from 'projects/ngx-mat-typeahead/src/public_api';
 
 @NgModule({
   declarations: [
@@ -11,8 +14,11 @@ import { ButtonModule } from '../../projects/button/src/lib/button.module';
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
     ComponentsModule,
-    ButtonModule
+    ButtonModule,
+    BrowserAnimationsModule,
+    NgxMatTypeaheadModule
   ],
   providers: [],
   bootstrap: [AppComponent]
